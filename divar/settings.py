669bdb704d19127a5b6910389012b9b36b69de94
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-07 18:16:22 +0430

    Update settings.py

--- divar/settings.py
+++ divar/settings.py
@@ -3,16 +3,7 @@ SECRET_KEY = 'django-insecure-vvq-h(a-3bqlzp^1f+)#zcxq7d590)pzi$@#9f@q%a+^i7@m#(
 DEBUG = False
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "ZnQhsP1JWGrzp3WCkmpcU9lLxiLlrmQq",
-        "HOST": "divardb.elyashedayat10.svc",
-        "PORT": "5432",
-    }
-}
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -66,6 +57,16 @@ ROOT_URLCONF = "divar.urls"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "ZnQhsP1JWGrzp3WCkmpcU9lLxiLlrmQq",
+        "HOST": "divardb.elyashedayat10.svc",
+        "PORT": "5432",
+    }
+}
 
 TEMPLATES = [
     {
