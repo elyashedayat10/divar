cdd83f0b686a8cca468d9efca408a411aa408286
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-08 06:34:32 +0430

    Update urls.py

--- divar/urls.py
+++ divar/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.template.defaulttags import url
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
