29ca08c9e7aa9490305ca31490076587d2747989
Elyas Hedayat <98589320+elyashedayat10@users.noreply.github.com>
2022-07-08 06:31:48 +0430

    Update settings.py

--- divar/settings.py
+++ divar/settings.py
@@ -312,18 +312,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
 
-MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media/"
+STATIC_ROOT = BASE_DIR / "static_cdn" / "static"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR / "static_cdn" / "media"
 
 
 
